f3667da160678bb122a4c312992069739a36627d
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2022-08-11 15:09:16 +0200

    Todo version of api added with get, create and update

--- README.md
+++ README.md
@@ -4,9 +4,34 @@ docker run -p 49160:4000 -d graphql/node-web-app
 <br>
 open index.html
 
+Warriors:
+
 {
 warriors {
 id
 name
+}
+}
+
+Todo:
+
+mutation {
+createTodo(input: {text: "eat"}) {
+id
+text
+}
+}
+
+mutation {
+updateTodo(id: "e99ce10750c93793a23d", input: {text: "eat"}) {
+id
+text
+}
+}
+
+query {
+getTodo(id: "0e127cfbe6f4ee92993b"){
+id
+text
 }
 }
--- server.js
+++ server.js
@@ -1,135 +1,54 @@
 import express from 'express'
-import cors from 'cors'
+import crypto from 'crypto'
 import { graphqlHTTP } from 'express-graphql'
-import { makeExecutableSchema } from '@graphql-tools/schema'
-
-const app = express()
-const port = 4000
-
-// In-memory data store
-const data = {
-  warriors: [
-    { id: '001', name: 'Jaime' },
-    { id: '002', name: 'Jorah' },
-  ],
+import { buildSchema } from 'graphql';
+const schema = buildSchema(`
+  input TodoInput {
+    text: String    
+  }
+  type Todo {
+    id: ID!
+    text: String    
+  }
+  type Query {
+    getTodo(id: ID!): Todo
+  }
+  type Mutation {
+    createTodo(input: TodoInput): Todo
+    updateTodo(id: ID!, input: TodoInput): Todo
+  }
+`);
+class Todo {
+  constructor(id, { text }) {
+    this.id = id;
+    this.text = text;
+  }
 }
-
-// Schema
-const typeDefs = `
-type Warrior {
-  id: ID!
-  name: String!
-}
-
-type Query {
-  warriors: [Warrior]
-}
-`
-
-// Resolver for warriors
-const resolvers = {
-  Query: {
-    warriors: (obj, args, context) => context.warriors,
+let todos = {};
+const root = {
+  getTodo: ({ id }) => {
+    if (!todos[id]) {
+      throw new Error('Todo not found.');
+    }
+    return new Todo(id, todos[id]);
   },
-}
-
-const executableSchema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
-})
-
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-// Entrypoint
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema: executableSchema,
-    context: data,
-    graphiql: true,
-  })
-)
-
-app.listen(port, () => {
-  console.log(`Running a server at http://localhost:${port}`)
-})
-
-// // 'use strict';
-// //
-// // const express = require('express');
-// // const cors = require('cors');
-// // import { graphqlHTTP } from 'express-graphql'
-// // import { makeExecutableSchema } from '@graphql-tools/schema'
-// //
-// // // Constants
-// // const PORT = 8080;
-// // const HOST = '0.0.0.0';
-// //
-// // // App
-// // const app = express();
-// // app.get('/', (req, res) => {
-// //   res.send('Hello World');
-// // });
-// //
-// // app.listen(PORT, HOST);
-// // console.log(`Running on http://${HOST}:${PORT}`);
-//
-// import express from 'express'
-// import cors from 'cors'
-// import { graphqlHTTP } from 'express-graphql'
-// import { makeExecutableSchema } from '@graphql-tools/schema'
-//
-// const data = {
-//   warriors: [
-//     { id: '001', name: 'Jaime' },
-//     { id: '002', name: 'Jorah' },
-//   ],
-// }
-//
-// const typeDefs = `
-// type Warrior {
-//   id: ID!
-//   name: String!
-// }
-//
-// type Query {
-//   warriors: [Warrior]
-// }
-// `
-//
-// const executableSchema = makeExecutableSchema({
-//   typeDefs,
-//   resolvers,
-// })
-//
-// const resolvers = {
-//   Query: {
-//     warriors: (obj, args, context, info) => context.warriors,
-//   },
-// }
-//
-// const app = express()
-// const port = 8080
-//
-// app.use(cors())
-// app.use(express.json())
-// app.use(express.urlencoded({ extended: true }))
-//
-// app.use(
-//   '/graphql',
-//   graphqlHTTP({
-//     schema: executableSchema,
-//     context: data,
-//     graphiql: true,
-//   })
-// )
-//
-// // app.get('/', (request, response) => {
-// //   response.send('Hello, GraphQL!')
-// // })
-//
-// app.listen(port, () => {
-//   console.log(`Running a server at http://localhost:${port}`)
-// })
+  createTodo: ({ input }) => {
+    const id = crypto.randomBytes(10).toString('hex');
+    todos[id] = input;
+    return new Todo(id, input);
+  },
+  updateTodo: ({ id, input }) => {
+    if (!todos[id]) {
+      throw new Error('Todo not found');
+    }
+    todos[id] = input;
+    return new Todo(id, input);
+  },
+};
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000, () => console.log('server started'));
